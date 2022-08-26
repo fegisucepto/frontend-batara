3e1661bf43fd5e105b166b4cd0c9c413d7101287
Fegi Sucepto Priawan <fegisucepto6997@gmail.com>
2022-08-26 14:21:32 +0700

    progress

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -8,6 +8,12 @@
           </b-card-text>
           <template #footer>
             <small class="text-muted">{{ data.updated_at }}</small>
+             <a
+            class="btn btn-danger px-4"
+            href="/activity-groups"
+            role="button"
+            >Delete</a
+          >
           </template>
         </b-card>
       </b-card-group>
